88d05cefc5e406e9e2ee577e36b25611a1a432aa
mystica-oracolo <chiara84.lavecchia@gmail.com>
2026-06-16 12:10:03 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // MYSTICA ORACOLI — Service Worker
 // Versione cache: incrementa questo valore ad ogni deploy
 // ============================================================
-const CACHE_NAME = 'mystica-v11';
+const CACHE_NAME = 'mystica-v12';
 
 // File da mettere in cache per il funzionamento offline
 const URLS_TO_CACHE = [
